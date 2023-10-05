34fa898878c017b369bbdc7ce913b0ae5ee9e84e
Md yasen rafe <mdyasenrafe@gmail.com>
2023-10-05 15:45:40 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { AppRegistry } from "react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import ProductListing from "./src/screens/ProductListing";
+import { useFonts } from "expo-font";
+
+const headers = {
+  "x-client-id": "651e5a8612e5356e23ce5a57",
+  "x-app-id": "651e5a8612e5356e23ce5a59",
+};
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: "https://api.chargetrip.io/graphql",
+  headers,
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
+  const [loaded] = useFonts({
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <ApolloProvider client={client}>
+      <ProductListing />
+    </ApolloProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
